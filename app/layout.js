c22b90889516e4a47dfcaa2cb69be42cad974b0a
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-07 06:02:51 +0900

    run buildしてみた

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
