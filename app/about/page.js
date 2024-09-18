cd5a98b0be5e7113f06a3d16096646203c81a1ff
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-18 13:20:52 +0900

    🎨 headerの追加

--- app/about/page.js
+++ app/about/page.js
@@ -2,13 +2,15 @@
 
 import styles from "../page.module.css";
 import { Footer } from "@/components/footer";
+import { Header } from "@/components/Header";
 
 import { Main } from "@/components/Main";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Main page="about" />
+      <Header />
+      <Main page="About" />
       <Footer />
     </div>
   );
