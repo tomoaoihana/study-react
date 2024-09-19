a986f1310ed0509a4f666367a57fc84e413d54ac
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-20 06:47:51 +0900

    🐛 軽微な修正

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 import styles from "src/app/page.module.css";
 import { useCallback } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const foo = "bar"; //引数を定義する場合は内部で定義する
@@ -18,9 +19,9 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      <Link href="/about" onClick={handleClick}>
         Click me
-      </a>
+      </LinK>
       <Main page="index" />
       <Footer />
     </div>
