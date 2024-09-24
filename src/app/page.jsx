f568da268ffa2e6c144c72aaf8da02197e33f875
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-25 07:01:42 +0900

    ✨ useStateで文字列と真偽値について

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,6 +9,8 @@ import { useCallback, useEffect, useState } from "react";
 export default function Home() {
   //分割代入で変数を定義する
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     //この書き方は非推奨
@@ -37,13 +39,27 @@ export default function Home() {
     //配列なので、幾つでも指定できる
   }, [count]);
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以上は入力できません");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   return (
     <div className={styles.page}>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button href="/about" onClick={handleClick} className={styles.btn}>
         Click me
       </button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
