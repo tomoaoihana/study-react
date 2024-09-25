d9f200765a6071b76f1514316f6dd6d79ba9e61e
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-26 06:55:56 +0900

    ✨ useStateで配列を作成

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,6 +11,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     //この書き方は非推奨
@@ -51,6 +52,23 @@ export default function Home() {
     setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      //スプレッド構文で配列を展開して新しい配列を作成する
+      //なぜスプレッド構文を使うのか？
+      //・配列・オブジェクトの破壊的メソッドは使わないために
+      //（NG）push, splice, unshift, pop, shift, reverse, sort
+      //⭐️勉強メモへ
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素は追加できません");
+        return prevArray;
+      }
+      // const newArray = [...prevArray, text];
+      // console.log(newArray === prevArray); //false
+      return [...prevArray, text];
+    });
+  }, [text]);
+
   return (
     <div className={styles.page}>
       <Header />
@@ -60,6 +78,12 @@ export default function Home() {
       </button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+      <button onClick={handleAdd}>追加する</button>
       <Main page="index" />
       <Footer />
     </div>
