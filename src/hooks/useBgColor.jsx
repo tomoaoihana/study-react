13e41af9af00de4da70317573426706b387c3c48
tomoaoihana <tamu.kusanishi@gmail.com>
2024-10-03 07:00:26 +0900

    ✨ useRouterを学ぶ

---
+++ src/hooks/useBgColor.jsx
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
+
+export const useBgColor = () => {
+  const router = useRouter();
+
+  console.log(router);
+
+  //useMemoは値をキャッシュする
+  //色の変更がある場合は再計算される
+  // const bgColor = useMemo(() => {
+  //   return router.pathname === "/" ? "lightblue" : "beige";
+  // }, [router.pathname]);
+
+  //switch文でも
+  //switch文とは？
+  //📝
+  const bgColor = useMemo(() => {
+    switch (router.pathname) {
+      case "/": {
+        return "lightblue";
+      }
+      case "/about": {
+        return "beige";
+      }
+      default: {
+        return "";
+      }
+    }
+  }, [router.pathname]);
+
+  useEffect(() => {
+    //DOMに直接アクセスする場合はuseEffectを使う
+    document.body.style.backgroundColor = bgColor; //マウント時に実行される
+    //クリーンアップ関数
+    return () => {
+      document.body.style.backgroundColor = ""; //アンマウント時に実行される
+    };
+
+    //配列なので、幾つでも指定できる
+  }, [bgColor]);
+};
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,12 +3,12 @@ import "src/app/globals.css";
 //hooks
 import { useCounter } from "src/hooks/useCounter";
 import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useBgColor } from "src/hooks/useBgColor";
 
 function MyApp({ Component, pageProps }) {
   const counter = useCounter(); //カスタムフックを呼び出す
   const inputArray = useInputArray(); //カスタムフックを呼び出す
-  useBgLightBlue(); //カスタムフックを呼び出す
+  useBgColor(); //カスタムフックを呼び出す
 
   return (
     <>
