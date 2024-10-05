8bcbaec86a3990ab9b93c849bd87ba56b5d998fa
tomoaoihana <tamu.kusanishi@gmail.com>
2024-10-06 07:01:03 +0900

    ✨ APIたたいてみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,40 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/Footer";
 import styles from "src/app/page.module.css";
 
 const Home = (props) => {
+  //⭐︎初期値を指定しないとエラーになる
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  console.log(posts);
+
   return (
     <div className={styles.page}>
       <Header />
-      <Main page="index" />
-      {props.isShow ? <h1>{props.count}</h1> : null}
-      <button href="/about" onClick={props.handleClick} className={styles.btn}>
-        Click me
-      </button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加する</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Footer />
+      <ol>
+        {posts.length > 0
+          ? posts.map((post) => {
+              return (
+                <li key={post.id}>
+                  <h2>{post.title}</h2>
+                  <p>{post.body}</p>
+                </li>
+              );
+            })
+          : "Loading..."}
+      </ol>
     </div>
   );
 };
