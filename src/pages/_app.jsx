0526f10296134ce7ff2ff274f2b5bf5027e753da
tomoaoihana <tamu.kusanishi@gmail.com>
2024-10-13 20:26:34 +0900

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,11 @@
 import "src/app/globals.css";
 
 //hooks
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgColor } from "src/hooks/useBgColor";
 
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter(); //カスタムフックを呼び出す
-  const inputArray = useInputArray(); //カスタムフックを呼び出す
-  useBgColor(); //カスタムフックを呼び出す
-
   return (
     <>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 };
