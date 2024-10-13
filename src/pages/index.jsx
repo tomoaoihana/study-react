c6c222eb317a04421a1c4fbc34d720261f180983
tomoaoihana <tamu.kusanishi@gmail.com>
2024-10-13 09:24:23 +0900

    Indexからふようなコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,23 @@
 "use client";
 
 import styles from "src/app/page.module.css";
-import { Footer } from "src/components/Footer"; // これを追加
-import { Main } from "src/components/Main";
+import Head from "next/head";
+
 import { Header } from "src/components/Header";
 
-const About = (props) => {
+const Index = () => {
   return (
     <div className={styles.page}>
+      <Head>
+        <title>Index Page</title>
+      </Head>
       <Header />
-      <Main page="About" />
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button href="/about" onClick={props.handleClick} className={styles.btn}>
-        Click me
-      </button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-      <button onClick={props.handleAdd}>追加する</button>
-
-      <Footer />
+      <div className={styles.inner}>
+        <h1>Next.jsで学ぶReact講座</h1>
+        <p>JSONPlaceholderのAPIを色々叩いてみるよ！</p>
+      </div>
     </div>
   );
 };
 
-export default About;
+export default Index;
