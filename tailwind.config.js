86f10482230e2c0cdee42c0bb4cff5be894ccebf
tomoaoihana <tamu.kusanishi@gmail.com>
2024-10-19 07:04:38 +0900

    ➕ tailwind css用のセットアップ

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
