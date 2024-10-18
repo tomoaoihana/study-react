cd31c68035b40b0c465171ee4c64d942581aaafa
tomoaoihana <tamu.kusanishi@gmail.com>
2024-10-18 16:28:23 +0900

    🚚 useFetchArrayの中を整理

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,26 +14,29 @@ const useFetchArray = (url) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
-export const useComments = () => {
-  return useFetchArray(`${API_URL}/comments`);
-};
-
+//posts
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
 };
 
+//users
 export const useUsers = () => {
   return useFetchArray(`${API_URL}/users`);
-};
-
-export const useCommentsByPostId = (id) => {
-  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
 };
 
 export const usePostsByUserId = (id) => {
   return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
 };
 
-export const usePostsByCommentId = (id) => {
-  return useFetchArray(id ? `${API_URL}/posts?id=${id}` : null);
+//comments
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`);
 };
+
+export const useCommentsByPostId = (id) => {
+  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
+};
+
+// export const usePostsByCommentId = (id) => {
+//   return useFetchArray(id ? `${API_URL}/posts?id=${id}` : null);
+// };
