b5a77612f8b8d86636b736bb7b2b4da4b05082c6
tomoaoihana <tamu.kusanishi@gmail.com>
2024-11-05 06:58:46 +0900

    ⚡️ コメントの個別ページをSG化した

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,13 +1,47 @@
 import { Header } from "src/components/Header";
-
 import Comment from "src/components/Comment";
+import { SWRConfig } from "swr";
 
-const CommentId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: { id: comment.id.toString() },
+  }));
+
+  return {
+    paths, //同じ名前の時は省略可能
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+
+  console.log(`comments/${id}のSGの処理が実行されました`);
+
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentId = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Header />
-
-      <Comment />
+      <SWRConfig value={{ fallback }}>
+        <Comment />
+      </SWRConfig>
     </div>
   );
 };
